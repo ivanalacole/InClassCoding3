fb3176f308e5297251ab0c273ae312f2eb0a4ca7
Ivana Burton Thompson <IvanaBT@R2Hs-MBP.lan>
2022-04-22 14:00:42 -0400

    pushing challenge

---
+++ index.js
@@ -0,0 +1,31 @@
+//1) Create 3 arrays, One with all String data types, one with all Numbers data types, and one with Boolean data types.  
+//You will need to add sample data to each array (don't over think this). //Each array should have at least 5 elements.. 
+
+//2) Next create a function that can take any of these arrays as a parameter 
+//and add an additional String, Number, or Boolean depending on which array was passed in. 
+
+//Hint: You will need an array method to add new items to the end of the array. 
+//You will also need conditionals set up to check the item at index 0 so you know 
+// what array was passed into the function.  
+//You can use the typeof method to determine what type of data is in the array.  
+
+//create arrays with string (names) number (intergers) true or false (boolean)
+let name = ['Ivana', 'Jeffrey', 'Dexter', 'Devonne', 'Candice']
+let birthYear = [1997, 1995, 1957, 1990, 1991]
+let social = Boolean([true, false, true, false, true])
+
+
+//create a function that passes through array 
+function socialSec (name, birthYear) {
+    if(name === birthYear){
+        console.log(social)
+    }else if(
+        name.push('Charlie'))
+    }else if(
+        birth.push(2008))
+    }}
+
+
+
+//adding data to an array you will have to use .push method 
+
